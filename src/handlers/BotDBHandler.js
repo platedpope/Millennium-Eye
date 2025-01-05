011d33ae6f54e18929b43416c7d699e30bf8bb48
platedpope <32043410+platedpope@users.noreply.github.com>
2025-01-04 20:44:08 -0500

    Fix Maliss TCGplayer price lookups
    Once again, TCGPlayer's inability to store names with special symbols strikes again. The bot looks cards up internally with their literal names (symbols and all), which doesn't match how TCGPlayer stores them on the API (without symbols), so cards like the Maliss series that have symbols in their name just get no price returns.

--- src/handlers/BotDBHandler.js
+++ src/handlers/BotDBHandler.js
@@ -106,7 +106,7 @@ function searchTcgplayerData(searches) {
 			// If this doesn't result in anything, resort to FTS to see if we can get a close match.
 			if (!(productRows.length)) {
 				// Adjust the name for FTS compatibility by removing weird symbols and double quoting quotes.
-				const ftsName = searchName.replace(/[^\x00-\x7F]/, ' ').trim().replace("\"", "\"\"")
+				const ftsName = searchName.replace(/[^\x00-\x7F]/g, ' ').trim().replace("\"", "\"\"").replace(/[<>]/g, '')
 				// FTS has a ton of (even ASCII) symbols it treats as special in non-string literals, so we want to convert this search to a string literal.
 				// Unfortunately string literals are treated as one search token which is pretty inflexible for search purposes, since anything that doesn't match it exactly doesn't hit.
 				// A bit hacky: split up the name on whitespace and quote each token to avoid issues with any symbols in each part. Add a * at the end of the last token for good measure to include prefixes.
